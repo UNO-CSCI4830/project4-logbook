3c35c9c7aca30edd82b17b93ee5838e92251e55e
Carlton Dickey <112052356+cedickey@users.noreply.github.com>
2025-11-26 18:11:09 -0600

    Added repository query method for finding applicances by alert date

--- backend/src/main/java/com/example/demo/repository/ApplianceRepository.java
+++ backend/src/main/java/com/example/demo/repository/ApplianceRepository.java
@@ -1,14 +1,20 @@
 package com.example.demo.repository;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Appliance;
 
 public interface ApplianceRepository extends JpaRepository<Appliance, Long> {
     List<Appliance> findAllByUserId(Long userId);
-    
+
     Optional<Appliance> findByUserIdAndId(Long userId, Long applianceId);
+
+    @Query("SELECT a FROM Appliance a WHERE DATE(a.alertDate) = DATE(:date)")
+    List<Appliance> findByAlertDate(@Param("date") Date date);
 }
